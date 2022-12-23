f58b51c8e87615b366056ee20fae41652583911d
Nick Knox <nick@knox.codes>
2022-12-22 16:03:37 -0800

    wallpaper

--- modules/configure-gnome.sh
+++ modules/configure-gnome.sh
@@ -6,3 +6,6 @@ sudo apt install -y gnome-shell-extension-autohidetopbar gnome-shell-extension-d
 # prefer adw-gtk3 theme
 wget -qO- "https://github.com/lassekongo83/adw-gtk3/releases/download/v4.1/adw-gtk3v4-1.tar.xz" | sudo tar xJ -C /usr/share/themes
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+# wallpaper
+gsettings set org.gnome.desktop.background picture-uri https://i.imgur.com/SBKjnxm.jpeg
