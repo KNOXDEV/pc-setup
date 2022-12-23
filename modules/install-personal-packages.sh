155dbb8ccb6eb3e2fdb9f7e4c6c6e2af228c40dc
Nick Knox <nick@knox.codes>
2022-12-22 16:18:08 -0800

    actuall chsh to zsh

--- modules/install-personal-packages.sh
+++ modules/install-personal-packages.sh
@@ -2,8 +2,7 @@
 sudo apt install -y audacity calf-plugins celluloid firefox-esr \
 	fonts-cantarell fonts-jetbrains-mono fonts-roboto \
 	gnome-shell-extension-manager kdenlive neofetch	neovim \
-	obs-studio texlive transmission-gtk virt-manager \
-	zsh zsh-autosuggestions zsh-syntax-highlighting
+	obs-studio texlive transmission-gtk virt-manager
 
 install_deb_url "https://discord.com/api/download?platform=linux&format=deb"
 install_deb_url "https://zoom.us/client/5.13.0.599/zoom_amd64.deb"
