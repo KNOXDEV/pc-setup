6ae58a9954ab89b6bbdb8bfd0ce6e4a43c8101d9
Nick Knox <nick@knox.codes>
2023-05-10 21:41:24 -0700

    helm repo

--- modules/install-terraform.sh
+++ modules/install-terraform.sh
@@ -9,9 +9,11 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list
 
 # 3. Update your package database and install terraform
-apt update && apt install -y terraform
+sudo apt update && sudo apt install -y terraform
 
 
 if ask_permission "install helm too?"; then
-  curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+  curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+  sudo apt-get update && sudo apt-get install helm
 fi
