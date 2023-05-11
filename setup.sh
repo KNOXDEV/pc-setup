0cb8e082814dec7e044369a3122b9e49420f6cc6
Nick Knox <nick@knox.codes>
2023-05-10 21:33:30 -0700

    now allows you to filter by modules and subroutines actually work

--- setup.sh
+++ setup.sh
@@ -30,10 +30,15 @@ sudo apt update && sudo apt install -y \
     curl \
     wget \
     gnupg \
-    lsb-release
+    lsb-release \
+    software-properties-common
 
 for mod in ./modules/*.sh; do
+	# you can pass an argument to filter scripts you're interested in
+	if [[ "$mod" != *"$1"* ]]; then
+		continue
+	fi
 	if ask_permission "run $(basename $mod)?"; then
-		bash $mod
+		. $mod
 	fi
 done
